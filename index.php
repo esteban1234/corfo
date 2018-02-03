5cdb74a155fe5cd2b560a240bf500114f462d584
esteban1234 <tva_libra@hotmail.com>
2018-02-02 18:03:53 -0600

    pagina avanzada

--- index.php
+++ index.php
@@ -1,6 +1,117 @@
 <?php include 'vistas/encabezado.php' ?>
 
 <body>
+	<figure class="logo">
+		<img src="img/corfo.png" alt="Corfo" class="img-responsive">
+	</figure>
+
+	<div class="menu">
+		<nav>
+			<ul>
+				<a class="select" href="">INICIO</a>
+				<a class="select" href="">NOSOTROS</a>
+				<a class="select" href="">SERVICIOS</a>
+				<a class="select" href="">CONTACTO</a>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="banner">
+		<div class="fondo">
+			<p>LIMPIEZA DE <br> CONFIANZA</p>
+		</div>
+	</div>
+
+
+	<div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="text-center">Algunos de Nuestro Servicios</h1>
+            <p class="text-center">Limpieza de Confianza</p>
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-md-2">
+            <img src="img/extintor.png" class=" img-responsive" style=" float: right;">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Services Pack</h3>
+            <p class="text-left">Manejo integral de plagas; técnicas de humo; tratamiento a unidades de transporte; Espolvoreo y nebulizaciones.</p>
+          </div>
+          <div class="col-md-2">
+            <img src="img/area2.png" class=" img-responsive" style=" float: right;">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Mantenimiento de áreas verdes</h3>
+            <p class="text-left">Desarrollamos procedimientos específicos para cada tipo de plaga, de tal forma que nuestros clientes reciben un servicio personalizado adaptado a sus necesidades.</p>
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-md-2">
+            <img src="img/lavadora.png" class=" img-responsive" style=" float: right;">
+          </div>
+          <div class="col-md-4">
+            <h3 class="text-left">Lavanderías y tintorerías</h3>
+            <p class="text-left">Servicio para el lavado de sus prendas de uso diario, a partir de 1 kg. Entregamos las prendas limpias, frescas y acomodadas.</p>
+          </div>
+          <div class="col-md-2">
+            <img src="img/bicho.png" class=" img-responsive" style=" float: right;">
+          </div>
+          <div class="col-md-4 text-center">
+            <h3 class="text-left">Servicios Especiales</h3>
+            <p class="text-left">Tratamiento de termitas, control de plagas a granos y semillas, sanitización y deodorización.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+<section style="background-color: #0093D4; overflow: hidden; padding: 5% 0%;">
+	<h1 class="text-center" style="color: #FFF;">Aplicaciones</h1>
+	<div class="index-content">
+	    <div class="container">
+	            <a href="blog-ici.html">
+	                <div class="col-lg-4">
+	                    <div class="card">
+	                        <div class="imagen">
+	                        	<!-- <img src="img/limpieza.jpg"> -->
+	                        </div>
+	                        <h4>Limpieza integral de inmuebles</h4>
+	                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	                        <a href="blog-ici.html" class="blue-button">Read More</a>
+	                    </div>
+	                </div>
+	            </a>
+	            <a href="blog-ici.html">
+	                <div class="col-lg-4">
+	                    <div class="card">
+	                        <div class="imagen2">
+	                        	<!-- <img src="img/limpieza.jpg"> -->
+	                        </div>
+	                        <h4>Mantenimiento y conservación de edificios</h4>
+	                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+	                        <a href="blog-ici.html" class="blue-button">Read More</a>
+	                    </div>
+	                </div>
+	            </a>
+	            <a href="blog-ici.html">
+	                <div class="col-lg-4">
+	                    <div class="card">
+	                        <div class="imagen3">
+	                        	<!-- <img src="img/limpieza.jpg"> -->
+	                        </div>
+	                        <h4>Control de plagas</h4>
+	                        <p>Desarrollamos programas para el control integral de plagas, a través del análisis del inmueble, condiciones geográficas y necesidades del cliente, identificando y controlando la plaga y las causas que la originan.</p>
+	                        <a href="blog-ici.html" class="blue-button">Read More</a>
+	                    </div>
+	                </div>
+	            </a>
+	
+	    </div>
+	</div>
+</section>
+
 
 <?php include 'vistas/pie.php' ?>
 
--- vistas/pie.php
+++ vistas/pie.php
@@ -1,5 +1,4 @@
 <footer>
-	<p class="textpie">Derechos de autor 2017© | VIGILANCIA, MANTENIMIENTO Y LIMPIEZA ESPECIALIZADA CORFO S.A. de C.V.</p>
+	<p class="textpie">Derechos de autor 2018© | VIGILANCIA, MANTENIMIENTO Y LIMPIEZA ESPECIALIZADA CORFO S.A. de C.V.</p>
 	<p class="textpie2">info@vigilanciacorfo.com</p>
-	<!-- <figure id="figurapie"><img src="../img/impezaa.png" class="img-responsive" alt=""></figure> -->
 </footer>
