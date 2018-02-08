4045c301b9d0605f329121597ea01d5ac4dc44fd
esteban1234 <tva_libra@hotmail.com>
2018-02-08 13:55:02 -0600

    solo falta el modal

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -2,9 +2,83 @@
 
 <body>
 
+	<figure class="logo">
+		<img src="../img/corfo.png" alt="Corfo" class="img-responsive">
+	</figure>
+
+	<div class="menu">
+		<nav>
+			<ul>
+				<a class="select" href="../index.php">INICIO</a>
+				<a class="select" href="nosotros.php">NOSOTROS</a>
+				<a class="select" href="servicios.php">SERVICIOS</a>
+				<a class="select" id="selector" href="contacto.php">CONTACTO</a>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="banner4">
+		<div class="fondo4">
+			<p><i class="fas fa-phone-volume"></i> CONTACTO</p>
+		</div>
+	</div>
+
+
+    <section class="container">
+    	<h3 class="tit">Para CORFO es muy importante mantener un rápido contacto con nuestros clientes. Si estas interesado en recibir información detallada, una cotización o solicitar servicio no dudes en ponerte en contacto con nosotros.</h3>
+		<div class="container-page">				
+			<div class="col-md-6">
+				<h3 class="dark-grey">Formulario de contacto</h3>
+				
+				<div class="form-group col-lg-12">
+					<label>Nombre completo</label>
+					<input type="" name="" class="form-control" id="" value="">
+				</div>
+				
+				<div class="form-group col-lg-6">
+					<label>Teléfono</label>
+					<input type="password" name="" class="form-control" id="" value="">
+				</div>
+				
+				<div class="form-group col-lg-6">
+					<label>Correo</label>
+					<input type="email" name="" class="form-control" id="" value="">
+				</div>
+								
+				<div class="form-group col-lg-12">
+					<label>Comentario</label>
+					<textarea  class="form-control" name="comentario" placeholder="Escribenos tu mensaje" required/></textarea>
+				</div>
+				
+							
+			
+			</div>
+		
+			<div class="col-md-6">
+				<h3 class="dark-grey">Datos de la empresa</h3>
+				<p>
+					<b>Dirección:</b> C. 5D #302-E ENTRE C. 20-A, COL. X-CUMPICH, MERIDA, YUCATAN. C.P. 97204
+				</p> <br>
+				<p>
+					<b>Correo:</b> info@vigilanciacorfo.com
+				</p> <br>
+				<p>
+					<b>Teléfono:</b> PENDIENTE
+				</p> <br>
+				<p>
+					<b>Horario:</b> Lunes a Viernes de 9 am a 6 pm
+				</p> <br>
+				
+				<button type="submit" class="btn btn-primary">VER UBICACION</button>
+			</div>
+		</div>
+	</section>
+
+
 <?php include 'pie.php' ?>
 
 <script src="../js/jquery.js"></script>
 <script src="../js/menu.js"></script>
+<script src="../js/modal.js"></script>
 </body>
 </html>
--- vistas/servicios.php
+++ vistas/servicios.php
@@ -23,35 +23,97 @@
 	</div>
 
 	<div class="section" style="padding: 5% 0%;">
+		<p class="texts">En CORFO sabemos que una compañía la constituyen las personas que trabajan en ella, por ello nos esforzamos para mantener altos estándares de calidad y eficiencia en nuestro personal. <br> <br>
+
+		El entrenamiento y la capacitación a todos los niveles, es una actividad constante, la responsabilidad que tenemos ante nuestros clientes nos lo exige, asegurándonos que siempre reciban un servicio profesional. <br><br>
+
+	    Los servicios integrados que ofrecemos son:</p>
       <div class="container">
         <div class="row">
           <div class="col-md-4">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
-            class="img-responsive">
-            <h2>A Title</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
-              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-              <br>Ut enim ad minim veniam, quis nostrud</p>
+            <div class="imagens">
+
+            </div>
+            <h2 class="text-center">Limpieza Y Mantenimiento</h2>
+            <p class="puntos">
+               <i class="far fa-hand-point-right"></i> Áreas de trabajo <br>
+               <i class="far fa-hand-point-right"></i> Áreas públicas <br>
+               <i class="far fa-hand-point-right"></i> Oficinas <br>
+               <i class="far fa-hand-point-right"></i> Albercas y jacuzzis <br>
+               <i class="far fa-hand-point-right"></i> Baños <br>
+               <i class="far fa-hand-point-right"></i> Yates <br>
+               <i class="far fa-hand-point-right"></i> Áreas verdes <br>
+               <i class="far fa-hand-point-right"></i> Exteriores <br>
+               <i class="far fa-hand-point-right"></i> Artículos de lujo <br>
+               <i class="far fa-hand-point-right"></i> Ventanales</p>
           </div>
           <div class="col-md-4">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
-            class="img-responsive">
-            <h2>A title</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
-              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-              <br>Ut enim ad minim veniam, quis nostrud</p>
+            <div class="imagens2">
+
+            </div>
+            <h2 class="text-center">Mantenimiento General</h2>
+            <p class="puntos">
+            	<i class="far fa-hand-point-right"></i> Rebrillantado pisos de mármol <br>
+				<i class="far fa-hand-point-right"></i> Pulido de piso <br>
+				<i class="far fa-hand-point-right"></i> Eliminación de sarro <br>
+				<i class="far fa-hand-point-right"></i> Lavado de áreas industriales <br>
+				<i class="far fa-hand-point-right"></i> Saneamiento <br>
+				<i class="far fa-hand-point-right"></i> Lavado en seco</p>
           </div>
           <div class="col-md-4">
-            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
-            class="img-responsive">
-            <h2>A title</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
-              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
-              <br>Ut enim ad minim veniam, quis nostrud</p>
+            <div class="imagens3">
+
+            </div>
+            <h2 class="text-center">Lavado Y Renovación</h2>
+            <p class="puntos">
+            	<i class="far fa-hand-point-right"></i> Muebles <br>
+				<i class="far fa-hand-point-right"></i> Alfombras <br>
+				<i class="far fa-hand-point-right"></i> Cortinas <br>
+				<i class="far fa-hand-point-right"></i> Colchones <br>
+				<i class="far fa-hand-point-right"></i> Ropa <br>
+				<i class="far fa-hand-point-right"></i> Colchonetas <br>
+				<i class="far fa-hand-point-right"></i> Sabanas <br>
+				<i class="far fa-hand-point-right"></i> Sobrecamas</p>
           </div>
         </div>
       </div>
     </div>
+
+
+    <section style="background-color: #0093D4; overflow: hidden; padding: 5% 0%;">
+	
+      <div class="container">
+      	<h1 class="text-center" style="color: #fff;">Servicios de protección y custodia mediante el monitoreo de sistemas de seguridad</h1> <br>
+	<p class="text-center lu" style="width: 65%; margin: 0 auto; color: #fff;">SOLUCIONES QUE TENEMOS PARA USTED</p> <br><br>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="iconos">
+            	<i class="far fa-hospital fa-7x"></i>
+            </div>
+            <h3 class="text-center" style="color: #fff;">SEGURIDAD COMERCIAL</h3>
+            <!-- <p>Llevar un archivo de grabaciones puede ayudarle a resolver los casos de incidentes o crímenes con las autoridades.</p> -->
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+            	<i class="fas fa-home fa-7x"></i>
+            </div>
+            <h3 class="text-center" style="color: #fff;">SEGURIDAD RESIDENCIAL</h3>
+            <!-- <p>Obtenga un monitoreo en tiempo real de las áreas críticas de su empresa u hogar.</p> -->
+          </div>
+          <div class="col-md-4">
+            <div class="iconos">
+            	<i class="far fa-building fa-7x"></i>
+            </div>
+            <h3 class="text-center" style="color: #fff;">SEGURIDAD PARA CORPORATIVOS</h3>
+            <!-- <p>Un sistema de videovigilancia activo no puede impedir un robo, pero hará pensar dos veces a los criminales antes de realizarlo, lo que reduce considerablemente el riesgo.</p> -->
+          </div>
+        </div>
+      </div>
+	
+	<div style="text-align: center; margin-top: 3%;">
+      <a class="btn btn-default btn-outline btn-lg" href="contacto.php"><i class="far fa-hand-point-right"></i> ¡CONTACTANOS!</a>
+  </div>
+</section>
 	
 
 <?php include 'pie.php' ?>
