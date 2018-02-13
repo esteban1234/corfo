0d7d6424d1679f011b20b072ec5bed7dfc624394
esteban1234 <tva_libra@hotmail.com>
2018-02-13 16:16:31 -0600

    cambios

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -7,7 +7,7 @@
 	<title>VIGILANCIA, MANTENIMIENTO Y LIMPIEZA ESPECIALIZADA CORFO</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   	<META NAME="Title" content="VIGILANCIA, MANTENIMIENTO Y LIMPIEZA ESPECIALIZADA CORFO">
-  	<META NAME="Author" CONTENT="[]"/>
+  	<META NAME="Author" CONTENT="[http://vigilanciacorfo.com/]"/>
   	<META name="Description" content="Somos una compañía dedicados a ofrecer servicios de limpieza y vigilancia."/>
   	<META NAME="Keywords" CONTENT="Limpieza, seguridad, organización de Limpieza, Limpieza de empresas y hogares, seguridad de empresas y hogares, coordinación de seguridad, empresas y hogares, renta de equipo para seguridad, venta de productos para limpieza."/>
   	<link rel="stylesheet" href="../css/bootstrap.css">
