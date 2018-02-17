38aedf87dfb6fd6f09f4e1f0f8052ca5ac80ef54
esteban1234 <tva_libra@hotmail.com>
2018-02-17 12:24:39 -0600

    metas

--- vistas/encabezado2.php
+++ vistas/encabezado2.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-
+<meta name="google-site-verification" content="yMeWAIopqw8SW4-FXmgzLsy6u0EcRXOHnUFOQOTEXZw" />
 	<link rel="icon" type="image/png" href="../img/favicon.png" />
 	<title>VIGILANCIA, MANTENIMIENTO Y LIMPIEZA ESPECIALIZADA CORFO</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
